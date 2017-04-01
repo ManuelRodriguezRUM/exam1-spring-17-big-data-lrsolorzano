cbeb425972b00fd0224ff517adfb72e66e5a4140
Leo Solorzano <lrsolorzano@gmail.com>
2017-04-01 18:18:22 -0400

    Part 1 Spark
    Exam 1

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,5 @@
-#Code here
+import csv
+datosStudents = sc.textFile("/home/leo/Files/studentsPR.csv")
+students=datosStudents.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[0])
+final=students.filter(lambda x: x[5] == 'F').filter(lambda x: x[2] == '71381')
+final.foreach(print)
