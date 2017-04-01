a531ca7a65b9a99880193d954f4347ca74b4d0ae
Leo Solorzano <lrsolorzano@gmail.com>
2017-04-01 18:09:06 -0400

    Hive Examen
    Problem 2 - Exam 1

--- hive/hive.sql
+++ hive/hive.sql
@@ -1,1 +1,18 @@
 # You sql follows
+select region, ciudad, count(*)
+    from escuelaspr
+    group by region, ciudad;
+
+ select esc.id, count(*)
+    from studentspr est, escuelaspr esc 
+    where est.id = esc.id 
+    group by esc.id;
+
+select esc.ciudad, esc.id, est.numEstudiante
+    from studentsPR est , escuelasPR  esc
+    where esc.id= est.id AND (esc.ciudad ="Ponce" OR esc.ciudad="Cabo Rojo");
+
+select esc.region, esc.ciudad, count(*)
+    from studentsPR est, escuelasPR esc
+    where est.id=esc.id 
+    group by esc.region, esc.ciudad;
