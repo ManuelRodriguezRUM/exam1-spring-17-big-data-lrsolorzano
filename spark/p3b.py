9f60271df229ca365dfc1910a36c6584752510c9
Leo Solorzano <lrsolorzano@gmail.com>
2017-04-01 18:29:28 -0400

    Parte 2 Spark
    Exam 1

--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,9 @@
-#Code here
+import csv
+datosEscuelas = sc.textFile("/home/leo/Files/escuelasPR.csv")
+escuelas=datosEscuelas.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[0])
+escuelasPonce = escuelas.filter(lambda x: x[2] == 'Ponce')
+escuelasCaboRojo = escuelas.filter(lambda x: x[2] == 'Cabo Rojo')
+escuelasDorado = escuelas.filter(lambda x: x[2] == 'Dorado')
+final2 = escuelasPonce.union(escuelasCaboRojo).union(escuelasDorado)
+final2.foreach(print)
+
